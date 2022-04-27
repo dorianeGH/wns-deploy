4a09495b3ac5dd08487bef885186344cc6a18515
Doriane <berdalado@gmail.com>
2022-04-27 12:02:01 +0200

    updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -37,7 +37,7 @@ function App() {
         <AddWilder />
       </Container>
       <Container>
-        <h2>Wilders</h2>
+        <h2>Wilderssssssssss</h2>
         <CardRow>
           {data?.wilders.map((wilder) => (
             <Wilder key={wilder._id} {...wilder} />
